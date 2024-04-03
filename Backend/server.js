ac2dc29a45c85602e383b385162a84ad9f2d773c
Your Name <you@example.com>
2024-04-03 13:57:58 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const path = require("path")
+
+const IndexRoute = require("./Routers/index")
+const connectDatabase = require("./Helpers/database/connectDatabase")
+const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
+
+dotenv.config({
+    path:  './Config/config.env'
+})
+
+connectDatabase()
+
+const app = express() ;
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/",IndexRoute)
+
+app.use(customErrorHandler)
+
+const PORT = process.env.PORT || 5000 ;
+
+app.use(express.static(path.join(__dirname , "public") ))
+
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static("Frontend/build"))
+}
+
+const server = app.listen(PORT,()=>{
+
+    console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`)
+
+})
+
+process.on("unhandledRejection",(err , promise) =>{
+    console.log(`Logged Error : ${err}`)
+
+    server.close(()=>process.exit(1))
+})
